c92115ec66cbdaea9bc8a30d2264786322356064
ValeriiaLebedeva <62835399+ValeriiaLebedeva@users.noreply.github.com>
2022-02-14 01:09:20 +0300

    add config, steps, openSettingsTest

---
+++ src/test/java/config/MobileConfig.java
@@ -0,0 +1,13 @@
+package config;
+
+import org.aeonbits.owner.Config;
+
+@Config.Sources({"classpath:config/browserstack.properties"
+})
+public interface MobileConfig extends Config {
+    String user();
+
+    String key();
+
+    String appUrl();
+}
